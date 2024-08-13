c137a5c21185a347e205de1bcd15eac62ff5535f
windlil <windlil@outlook.com>
2024-08-13 14:46:36 +0800

    chore: update routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -22,7 +22,25 @@ export const routes: RouteObject[] = [
   },
   {
     path: '/home',
-    element: createLazyElement(() => import('@/views/Home/index'))
+    element: createLazyElement(() => import('@/views/Home/index')),
+    children: [
+      {
+        path: '/home/app-list',
+        element: createLazyElement(() => import('@/views/Home/AppList'))
+      },
+      {
+        path: '/home/page-list',
+        element: createLazyElement(() => import('@/views/Home/PageList'))
+      },
+      {
+        path: '/home/component-list',
+        element: createLazyElement(() => import('@/views/Home/ComponentList'))
+      },
+      {
+        path: '/home/user',
+        element: createLazyElement(() => import('@/views/Home/User'))
+      },
+    ]
   },
   {
     path: '/editor',
